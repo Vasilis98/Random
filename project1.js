90a8089b9e0ce401dff1324cf53c85a526bfac54
VasilisN <billnick@hotmail.gr>
2024-04-30 17:16:26 +0300

    Your commit message here

---
+++ project1.js
@@ -0,0 +1,7 @@
+const quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Believe you can and you are halfway there. - Theodore Roosevelt",
+    "In the middle of difficulty lies opportunity. - Albert Einstein",
+    "You miss 100% of the shots you dont take. -Wayne Gretzky"
+];
+
